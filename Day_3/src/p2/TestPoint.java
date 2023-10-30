905044be35ae9dfc5cc275dd6372460c1c83c483
SadhanaBorhade <146407065+SadhanaBorhade@users.noreply.github.com>
2023-10-30 13:40:38 +0530

    Add files via upload

---
+++ Day_3/src/p2/TestPoint.java
@@ -0,0 +1,33 @@
+package p2;
+import java.util.Scanner;
+import p1.Point2D;
+class TestPoint{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int x,y;
+		int x1,y1;
+		boolean flag;
+		System.out.println("Enter two co-ordinates for point 1:");
+		x=sc.nextInt();
+		y=sc.nextInt();
+		Point2D p1=new Point2D(x,y);
+		System.out.println("Enter two co-ordinates for point 2:");
+		x1=sc.nextInt();
+		y1=sc.nextInt();
+		Point2D p2=new Point2D(x1,y1);
+		
+		System.out.println(p1.show());
+		System.out.println(p2.show());
+		flag=p1.isEqual(p2);
+		if(flag)
+		{
+			System.out.println("Both points are same");
+		}
+		else
+		{
+			System.out.println("Both points are Different ");
+			System.out.println("Distance between 2 points is:" +p1.getDistance(p2));
+		}
+	}
+}
